92aab92fa236dbdc4fc9ce48ffcf7f2c94f8a321
tomuyamukun <3156tomochan@gmail.com>
2021-10-18 00:54:39 +0900

    マウスイベント

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,24 @@ import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import styles from "../styles/Home.module.css";
+import React, { useCallback } from "react";
 
 export default function Home() {
+	const buttonClick = useCallback(
+		(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+			e.preventDefault();
+			alert("Click");
+		},
+		[]
+	);
+
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Create Next App</title>
 			</Head>
 			<Header />
+			<button onClick={buttonClick}>ボタン</button>
 			<Main page="index" />
 			<Footer />
 		</div>
