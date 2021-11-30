466c50c62b70a0f304793f64db55d8aae845dc7f
tomuyamukun <3156tomochan@gmail.com>
2021-11-30 23:08:36 +0900

    🔥 不必要なファイルやコードを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,11 @@
 import "src/styles/globals.css";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgColor } from "src/hooks/useBgColor";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-	const counter = useCounter();
-	const inputArray = useInputArray();
-	useBgColor();
-
 	return (
 		<>
-			<Component {...pageProps} {...counter} {...inputArray} />
+			<Component {...pageProps} />
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
