4971d9c7c855419efb0c93ca9bc0bf0595d5539b
tomuyamukun <3156tomochan@gmail.com>
2021-11-30 23:05:32 +0900

    🔥 indexから不要なコードの削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
 type props = {
@@ -15,29 +13,16 @@ type props = {
 	handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const About = (props: props) => {
+const Index = (props: props) => {
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Create Next App</title>
 			</Head>
 			<Header />
-			{props.isShow ? <h1>{props.count}</h1> : null}
-			<button onClick={props.handleClick}>ボタン</button>
-			<button onClick={props.handleDisplay}>
-				{props.isShow ? "非表示" : "表示"}
-			</button>
-
-			<input type="text" value={props.text} onChange={props.handleChange} />
-			<button onClick={props.handleAdd}>追加</button>
-			<ul>
-				{props.array.map((item) => {
-					return <div key={item}>{item}</div>;
-				})}
-			</ul>
-			<Main page="about" />
-			<Footer />
+			<h1>Next.jsで学ぶReact講座</h1>
+			<p>JSONPlaceholderで色々叩く</p>
 		</div>
 	);
 };
-export default About;
+export default Index;
